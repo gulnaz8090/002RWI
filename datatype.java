56366e301e1b8fda78e93b04ce1d1bef25d68e77
gulnaz8090 <gulnazansari502@gmail.com>
2024-05-30 13:05:29 +0530

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,42 @@
+class DT{
+    public static void main(String args[]){
+    //numeric datatype.
+    byte byteval;
+    short shortval;
+    int intval;
+    long longval;
+
+    //REAL VARIABLE
+    float floatval;
+    double doubleval;
+
+    //CHARACTER VARIABLE
+    char charval;
+
+    //BOOLEAN VARIABLE
+    boolean boolval;
+
+    byteval = 10;
+    shortval = 100;
+    intval = 10000;
+    longval = 800000000;
+    floatval = 10.50f;
+    doubleval = 675768678.87678677;
+    charval = 'A';
+    boolval = true;
+
+    System.out.println("Byte Val = "+byteval);
+    System.out.println("Short Val = "+shortval);
+    System.out.println("Int Val = "+intval);
+    System.out.println("Long Val = "+longval);
+    System.out.println("Float Val = " +floatval);
+    System.out.println("Double Val = "+doubleval);
+    System.out.println("Char Val = "+charval);
+    System.out.println("Boolean Val = "+boolval);
+
+
+
+
+
+   }
+}
