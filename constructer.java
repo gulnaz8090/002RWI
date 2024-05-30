cc10a8c01835291276119f09638bb76f31a4e050
gulnaz8090 <gulnazansari502@gmail.com>
2024-05-30 13:06:02 +0530

    Add files via upload

---
+++ constructer.java
@@ -0,0 +1,11 @@
+class Demo {
+    Demo(String name){
+        System.out.println("Hello From Demo To"+name);
+    
+    }
+}
+class Mainclass{
+    public static void main (String args[]){
+        Demo d = new Demo("Gulnaz");
+    }
+}
